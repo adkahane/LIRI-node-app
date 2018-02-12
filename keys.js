db53f0abe4a098cf851b193ecfdd79e32bb2ba00
adkahane <adam.kahane.nj@gmail.com>
2018-02-11 20:34:30 -0800

    skeleton or all required files have been created

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'fBKJuS6fDjg7LiDGnNP9ynSFW',
+  consumer_secret: 'nNc2mXWjQKmpapaHGayDzBzTafm3qELsN1zvMYq3UWKhSx9UUK',
+  access_token_key: '962901151191506944-vG0wLvSlyyurPCJLqNhpQQAnxdzNrgN',
+  access_token_secret: 'RzEQz4Cg4xsfiqDr56RgCwDynFC94SW5t3lWghXDozWqa',
+}
+
+module.exports = twitterKeys;
