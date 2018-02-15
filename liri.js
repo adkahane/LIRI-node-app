3e7f3e501936811cb075c49ec20a64eef978f737
adkahane <adam.kahane.nj@gmail.com>
2018-02-14 21:25:38 -0800

    Working version. Fixed log file format, added validation to catch undefined OMDB calls. cleaned, added comments.

--- liri.js
+++ liri.js
@@ -8,14 +8,14 @@ var command = process.argv[2];
 var songName = "the sign ace of base";
 var movieName = "Mr Nobody";
 
-// Gets twitter keys and tokens
+// Twitter keys and tokens
 var client = new twitter({
     consumer_key: keys[0].consumer_key,
     consumer_secret: keys[0].consumer_secret,
     access_token_key: keys[0].access_token_key,
     access_token_secret: keys[0].access_token_secret
 });
-// Gets spotify keys and tokens
+// Spotify keys and tokens
 var spotify = new Spotify({
     id: keys[1].client_id,
     secret: keys[1].client_secret
@@ -41,12 +41,8 @@ function logic(){
     }        
 }
 
+// Displays last 20 tweets when called
 function myTweets() {
-    fs.appendFile("./log.txt", "----------------\n" + command + "\n----------------\n", function(err){
-        if(err){
-            throw err;
-        }
-    });
     client.get("statuses/user_timeline", params, function(error, tweets, response){
         if(error){
             return (error);
@@ -54,13 +50,9 @@ function myTweets() {
         for(var i = 0; i < tweets.length; i++){
             console.log(tweets[i].text);
             console.log(tweets[i].created_at, "\n");
-            fs.appendFile("./log.txt", tweets[i].text + "\n" + tweets[i].created_at + "\n\n", function(err){
-                if(err){
-                    throw err;
-                }
-            });
         }
-        fs.appendFile("./log.txt", "\n--------------------------------------------------------------------", function(err){
+        // Writes to log.txt file
+        fs.appendFile("./log.txt", "\n" + new Date().toISOString().replace('T', ' ').substr(0, 19) + " ==> looked at your last 20 Tweets\n--------------------------------------------------------------------", function(err){
             if(err){
                 throw err;
             }
@@ -68,7 +60,10 @@ function myTweets() {
     });
 }
 
+// Displays information on 10 songs with the queried name
 function spotifyThis() {
+
+    // Gets user input if available
     if (process.argv.length > 3) {
         songName = "";
         for (var i = 3; i < process.argv.length; i++){
@@ -81,18 +76,14 @@ function spotifyThis() {
         }
         console.log("Song Name: " + songName +"\n");
         console.log("Versions:\n");
+
+        // Loop to display all songs
         for (var i = 0; i < data.tracks.items.length; i++) {
             console.log((i+1) + ".\n" + "Band: " + data.tracks.items[i].album.artists[0].name);
             console.log("Album: " + data.tracks.items[i].album.name);
             console.log("Preview URL: " + data.tracks.items[i].external_urls.spotify + "\n");
-            
-            fs.appendFile("./log.txt", (i+1) + ".\n" + "Band: " + data.tracks.items[i].album.artists[0].name + "\nAlbum: " + data.tracks.items[i].album.name + "\nPreview URL: " + data.tracks.items[i].external_urls.spotify + "\n", function(err){
-                if(err){
-                    throw err;
-                }
-            });
         }
-        fs.appendFile("./log.txt", "\n--------------------------------------------------------------------", function(err){
+        fs.appendFile("./log.txt", "\n" + new Date().toISOString().replace('T', ' ').substr(0, 19) + " ==> looked up '" + songName + "' on Spotify\n--------------------------------------------------------------------", function(err){
             if(err){
                 throw err;
             }
@@ -100,6 +91,7 @@ function spotifyThis() {
     });
 }
 
+// Displays OMDB information on queried movie title
 function movieThis(){
     if (process.argv.length > 3){
         movieName = "";
@@ -109,23 +101,43 @@ function movieThis(){
     }
 
     request("http://www.omdbapi.com/?t=" + movieName + "&apikey=40e9cece", function(error, response, body) {
-
+        // Check for error from API call
         if (!error && response.statusCode === 200) {
+            // Check to see if the movie input was found
             if(JSON.parse(body).Response === "False"){
                 console.log(JSON.parse(body).Error);
+            }
+            // Check to see if any of the individual data elements return 'undefined'
+            else if (typeof JSON.parse(body).Title === "undefined" || typeof JSON.parse(body).Year === "undefined" || typeof JSON.parse(body).Ratings[0] === "undefined" || typeof JSON.parse(body).Ratings[1] === "undefined" || typeof JSON.parse(body).Country === "undefined" || typeof JSON.parse(body).Language === "undefined" || typeof JSON.parse(body).Plot === "undefined" || typeof JSON.parse(body).Actors === "undefined") {
+                console.log("Sorry, missing information on that title");
             }
             else {
                 console.log("Movie Title: " + JSON.parse(body).Title + "\nYear: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nMade in: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors);
             }
         }
+        fs.appendFile("./log.txt", "\n" + new Date().toISOString().replace('T', ' ').substr(0, 19) + " ==> Looked up the movie '" + movieName + "' on OMDB\n--------------------------------------------------------------------", function(err){
+            if(err){
+                throw err;
+            }
+        });
     });
 
 }
 
+// When called, checks the random.txt file and makes a new call based on it's contents
 function doThis(){
+    fs.appendFile("./log.txt", "\n" + new Date().toISOString().replace('T', ' ').substr(0, 19) + " ==> Ran the do-what-it-says function\n--------------------------------------------------------------------", function(err){
+        if(err){
+            throw err;
+        }
+    });
+    
     fs.readFile("./random.txt", "utf8", function(err, data){
+        // Split the contents of random.txt at the ','
         var random = data.split(",");
+        // Makes command equal to the first half of the text in random.txt
         command = random[0];
+        //Conditionals to set songName and movieName to their new values if those functions will be called by random.txt
         if (command === "spotify-this-song"){
             songName = random[1];
             logic();
@@ -140,5 +152,5 @@ function doThis(){
     });
 }
 
-// Initially runs the program
+// Initiall check to see which function to call
 logic();
